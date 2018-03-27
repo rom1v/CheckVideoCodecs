1adf10a32819a95fdc87136e03bb0c6c42fef8a9
Romain Vimont <rom@rom1v.com>
2018-03-27 08:55:45 +0200

    List and test encoders

--- app/src/main/java/com/rom1v/checkvideocodecs/Main.java
+++ app/src/main/java/com/rom1v/checkvideocodecs/Main.java
@@ -1,8 +1,67 @@
 package com.rom1v.checkvideocodecs;
 
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String... args) {
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                e.printStackTrace();
+            }
+        });
 
+        testEncoderByType("video/avc");
+        listCodecInfos();
+    }
+
+    private static void listCodecInfos() {
+        MediaCodecList list = new MediaCodecList(MediaCodecList.ALL_CODECS);
+        for (MediaCodecInfo codecInfo : list.getCodecInfos()) {
+            if (codecInfo.isEncoder()) {
+                printCodecInfo(codecInfo);
+            }
+        }
+    }
+
+    private static void printCodecInfo(MediaCodecInfo codecInfo) {
+        System.out.println("\n=== " + codecInfo.getName() + " ===");
+        String[] supportedTypes = codecInfo.getSupportedTypes();
+        System.out.println("Supported types: " + Arrays.toString(supportedTypes));
+        if (Arrays.asList(supportedTypes).contains("video/avc")) {
+            MediaCodecInfo.CodecCapabilities capabilities = codecInfo.getCapabilitiesForType("video/avc");
+            MediaCodecInfo.VideoCapabilities videoCapabilities = capabilities.getVideoCapabilities();
+            System.out.println("video/avc capabilities:");
+            System.out.println("    bitrate: " + videoCapabilities.getBitrateRange());
+            System.out.println("    framerate: " + videoCapabilities.getSupportedFrameRates());
+            testCodecByName(codecInfo.getName());
+        }
+    }
+
+    private static void testCodecByName(String codecName) {
+        try {
+            MediaCodec codec = MediaCodec.createByCodecName(codecName);
+            codec.release();
+            System.out.println("TEST OK");
+        } catch (Exception e) {
+            System.out.println("TEST KO");
+            e.printStackTrace();
+        }
+    }
+
+    private static void testEncoderByType(String type) {
+        try {
+            MediaCodec codec = MediaCodec.createEncoderByType(type);
+            System.out.println("video/avc -> " + codec.getCodecInfo().getName());
+            codec.release();
+        } catch (Exception e) {
+            System.out.println("Cannot create encoder by type");
+            e.printStackTrace();
+        }
     }
 }
